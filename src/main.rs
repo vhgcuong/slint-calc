1950bf4de29e99f85fc27d6e97c0b408aefd50e5
Yotoko <vhgcuong95@gmail.com>
2024-05-24 23:05:30 +0700

    GUI calc

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ slint::slint!{
         in property <string> text;
         min-height: 30px;
         min-width: 30px;
+        in property <brush> background: #1d78d3;
         Rectangle {
-            background: ta.pressed ? red : ta.has-hover ? #26bfb3 : #1d78d3;
+            background: ta.pressed ? red : ta.has-hover ? background.darker(10%) : background;
             animate background {duration: 100ms;}
             border-radius: 5px;
             border-width: 2px;
@@ -30,7 +31,7 @@ slint::slint!{
     }
 
     export component App inherits Window {
-        in property <int> value: 1;
+        in property <int> value: 0;
         GridLayout {
             spacing: 10px;
             padding: 20px;
@@ -42,24 +43,24 @@ slint::slint!{
                 Button { text: "1"; }
                 Button { text: "2"; }
                 Button { text: "3"; }
-                Button { text: "+"; }
+                Button { text: "+"; background: yellow; }
             }
             Row {
                 Button { text: "4"; }
                 Button { text: "5"; }
                 Button { text: "6"; }
-                Button { text: "-"; }
+                Button { text: "-"; background: yellow; }
             }
             Row {
                 Button { text: "7"; }
                 Button { text: "8"; }
                 Button { text: "9"; }
-                Button { text: "*"; }
+                Button { text: "*"; background: yellow; }
             }
             Row {
                 Button { text: "0"; colspan: 2;}
-                Button { text: "="; col: 2;}
-                Button { text: "/";}
+                Button { text: "="; col: 2; background: green; }
+                Button { text: "/"; background: yellow;}
             }
         }
         
@@ -80,10 +81,30 @@ fn main() {
 
     app.global::<CalcLogic>().on_button_pressed(move |value| {
         let app = weak.unwrap();
+        let mut state = state.borrow_mut();
         if let Ok(val) = value.parse::<i32>() {
-            
+            state.current_value *= 10;
+            state.current_value += val;
+            app.set_value(state.current_value);
+            return;
         }
-        app.set_value(app.get_value() * 10 + val);
+        if value.as_str() == "=" {
+            let result = match state.operator.as_str() {
+                "+" => state.prev_value + state.current_value,
+                "-" => state.prev_value - state.current_value,
+                "*" => state.prev_value * state.current_value,
+                "/" => state.prev_value / state.current_value,
+                _ => return,
+            };
+            app.set_value(result);
+            state.current_value = 0;
+            state.prev_value = result;
+            state.operator = Default::default()
+        } else {
+            state.operator = value.clone();
+            state.prev_value = state.current_value;
+            state.current_value = 0;
+        }
     });
     app.run().unwrap();
     println!("Hello, world!");
